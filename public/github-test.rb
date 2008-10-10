24b3c3eecef7ab5529de7cffa223fe04fff4f4a9
webmat <webmat@gmail.com>
2008-10-10 06:37:45 -0700

    Adds support for YAML dumps. GitHub supports them, now the test script does as well :-) (explanation http://github.com/blog/177-has-my-gem-built-yet)

--- public/github-test.rb
+++ public/github-test.rb
@@ -1,13 +1,22 @@
 #!/usr/bin/env ruby
-
+ 
+# An improved GitHub gemspec checker.
+# GitHub accepts YAML dumps for the gemspecs, which helps keep your gem generation tasks DRY
+ 
 if ARGV.size < 1
   puts "Usage: github-test.rb my-project.gemspec"
   exit
 end
-
+ 
 require 'rubygems/specification'
 data = File.read(ARGV[0])
 spec = nil
-Thread.new { spec = eval("$SAFE = 3\n#{data}") }.join
+begin
+  Thread.new { spec = eval("$SAFE = 3\n#{data}") }.join
+rescue SyntaxError
+  #Output in YAML format, then?
+  require 'yaml'
+  Thread.new { spec = YAML.load(data) }.join
+end
 puts spec
 puts "OK"
